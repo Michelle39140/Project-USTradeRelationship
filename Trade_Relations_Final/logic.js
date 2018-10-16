37d9428a4f4f5008975d59c9ec9565a0bd3f39ec
sanhita0910 <37850171+sanhita0910@users.noreply.github.com>
2018-10-16 09:19:02 -0400

    Add files via upload

--- Trade_Relations_Final/logic.js
+++ Trade_Relations_Final/logic.js
@@ -1,12 +1,12 @@
 // Create a map object
 var myMap = L.map("map", {
  center: [15.5994, -28.6731],
- zoom: 3
+ zoom: 2
 });
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
- maxZoom: 18,
+ maxZoom: 2,
  id: "mapbox.streets-basic",
  accessToken: API_KEY
 }).addTo(myMap);
---
+++ Trade_Relations_Final/nav.js
@@ -0,0 +1,20 @@
+document.getElementById("navMenu").innerHTML =
+
+
+      '<div class="w3-row w3-padding w3-black">'+
+        '<div class="w3-col s3">'+
+          '<a href="trade_relations_home.html" class="w3-button w3-block w3-black">HOME</a>'+
+        '</div>'+
+        '<div class="w3-col s3">'+
+          '<a href="map.html" class="w3-button w3-block w3-black">Trade Map</a>'+
+        '</div>'+
+        '<div class="w3-col s3">'+
+          '<a href="trade_flow.html" class="w3-button w3-block w3-black">Trade Flow</a>'+
+        '</div>'+
+        '<div class="w3-col s3">'+
+          '<a href="search.html" class="w3-button w3-block w3-black">Search the Data</a>'+
+        '</div>'+
+      '</div>';
+    
+
+ 
