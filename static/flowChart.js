09de75227ca80e6d5290ee29894ba48da8dde979
Michelle39140 <fanwusi.pku@gmail.com>
2018-10-16 20:33:51 -0400

    upload final version(with trend page)

--- static/flowChart.js
+++ static/flowChart.js
@@ -1,3 +1,5 @@
+// this script is modified from the script created by Luz K. Molina, http://bl.ocks.org/databayou/c7ac49a23c275f0dd7548669595b8017#regionsfish.csv
+
 /*** Define parameters and tools ***/
 var width = 760,
     height = 820,
